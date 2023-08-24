b667753c3ea80e47c3c278477e8449ab62db748c
DandyDrop <danilko223zaharov@gmail.com>
2023-08-24 22:19:35 +0300

    Add setup.py to be able to pip install this as python package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('requirements-repl.txt', 'r') as f:
+    requirements = f.readlines()
+
+setuptools.setup(
+    name='make_voices_louder_tg',
+    version='0.1.0',
+    author='DandyDrop',
+    description='Helps when you need to hear what somebody said in telegram voice note',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/DandyDrop/make_voices_louder_tg',
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent',
+    ],
+    keywords='make voice note louder telegram',
+    install_requires=requirements
+)
